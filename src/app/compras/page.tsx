f5728e986a5de743c8c956020cda6317bfaca0c4
VictorHugo55 <davichugopereira@gmail.com>
2024-10-29 08:30:43 -0300

    primeiro commit

---
+++ src/app/compras/page.tsx
@@ -0,0 +1,8 @@
+export default function Compras(){
+    
+    return(
+        <main className="flex justify-center p-10">
+            <h1 className="text-center text-4xl font-bold text-indigo-800">compras</h1>
+        </main>
+    );
+}
